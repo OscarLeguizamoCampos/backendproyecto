784810e4a565534dfe19dc5dfecf4a35d175544c
OscarLeguizamoCampos <oleguizamoc27@misena.edu.co>
2021-12-07 03:43:26 -0500

    Se modifico la base de datos en algunos campos

--- controllers/articulos/controller.js
+++ controllers/articulos/controller.js
@@ -10,8 +10,11 @@ const queryAllArticles = async (callback) => {
 const crearArticulo = async (datosArticulo, callback) => {
   if (
     Object.keys(datosArticulo).includes('name') &&
-    Object.keys(datosArticulo).includes('brand') &&
-    Object.keys(datosArticulo).includes('model')
+    Object.keys(datosArticulo).includes('category') &&
+    Object.keys(datosArticulo).includes('description') &&
+    Object.keys(datosArticulo).includes('precio') &&
+    Object.keys(datosArticulo).includes('unit') &&
+    Object.keys(datosArticulo).includes('cantidad')
   ) {
     const baseDeDatos = getDB();
     // implementar código para crear Artículo en la BD
